f4f9e6e189a5a2c5c8cfec1cd9c6eef372e1c5b8
Vanya Kojouharova <vanya.kojouharova@gmail.com>
2022-04-16 17:02:49 +0200

    bookmarks routing, controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:update, :destroy]
+
+  def new
+    @bookmark = List.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @bookmark, notice: 'Bookmark was sucessfully created'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_path, notice: 'Bookmark was sucessfully deleted'
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
