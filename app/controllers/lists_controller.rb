6ab98d2e158ae08b0707210744fa9552103cd398
Vanya Kojouharova <vanya.kojouharova@gmail.com>
2022-04-16 16:37:24 +0200

    Lists controller done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,32 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit]
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'List was sucessfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
